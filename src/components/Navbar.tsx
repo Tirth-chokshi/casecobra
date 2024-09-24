76cd7ab7107c8bacd2006d00898d625f5628e119
Tirth chokshi <chokshitirth4@gmail.com>
2024-09-24 21:02:35 +0530

    navbar done almost

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,86 @@
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import Link from "next/link"
+import { ArrowRight } from "lucide-react"
+import { buttonVariants } from "./ui/button"
 const Navbar = () => {
-    const user =  undefined
+    const user = undefined
+    const isAdmin = false
     return <nav className="sticky z-[100] h-14 inset-x-0 top-0 border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <MaxWidthWrapper>
             <div className="flex h-14 items-center justify-between border-b border-zinc-200">
                 <Link href={'/'} className="flex z-40 font-semibold">
-                case <span className="text-green-600">cobra</span>
+                    case <span className="text-green-600">cobra</span>
                 </Link>
 
                 <div className="h-full flex items-center space-x-4">
-                    {user? (
+                    {user ? (
                         <>
-                        {/* jsx for looged in user */}
-                        <Link href={'/api/auth/logout'}></Link>
+                            {/* jsx for looged in user */}
+                            <Link
+                                href={'/api/auth/logout'}
+                                className={buttonVariants({
+                                    size: 'sm',
+                                    variant: 'ghost'
+                                })}>
+                                Sign Out
+                            </Link>
+                            {
+                                isAdmin ?
+                                    <Link
+                                        href={'/api/auth/logout'}
+                                        className={buttonVariants({
+                                            size: 'sm',
+                                            variant: 'ghost'
+                                        })}>
+                                        Dashboard ✨
+                                    </Link>
+                                    : null
+                            }
+                            <Link
+                                href={'/configure/upload'}
+                                className={buttonVariants({
+                                    size: 'sm',
+                                    className: 'hidden sm:flex items-center gap-1'
+                                })}>
+                                Create case
+                                <ArrowRight className='ml-1.5 h-5 w-5'></ArrowRight>
+                            </Link>
                         </>
-                    ): (
+                    ) : (
                         <>
-                        {/* jsx for non user */}
-                        <Link href={'/api'}></Link>
+                            {/* jsx for non user */}
+                            <Link
+                                href={'/api/auth/register'}
+                                className={buttonVariants({
+                                    size: 'sm',
+                                    variant: 'ghost'
+                                })}>
+                                Sign up
+                            </Link>
+
+                            <Link
+                                href={'/api/auth/login'}
+                                className={buttonVariants({
+                                    size: 'sm',
+                                    variant: 'ghost'
+                                })}>
+                                Login
+                            </Link>
+                            <div className="h-8 w-px bg-zinc-200 hidden sm:block"></div>
+                            <Link
+                                href={'/configure/upload'}
+                                className={buttonVariants({
+                                    size: 'sm',
+                                    className: 'hidden sm:flex items-center gap-1'
+                                })}>
+                                Create case
+                                <ArrowRight className='ml-1.5 h-5 w-5'></ArrowRight>
+                            </Link>
                         </>
                     )}
                 </div>
             </div>
-            
+
         </MaxWidthWrapper>
     </nav>
 
