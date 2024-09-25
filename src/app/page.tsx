bc2a84963a5d24305c273cbc75a4f9bc965f2357
Tirth chokshi <chokshitirth4@gmail.com>
2024-09-25 20:37:43 +0530

    review componnet added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Icons } from '@/components/Icons'
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Phone from '@/components/Phone'
-// import { Reviews } from '@/components/Reviews'
+import { Reviews } from '@/components/Reviews'
 import { buttonVariants } from '@/components/ui/button'
 import { ArrowRight, Check, Star } from 'lucide-react'
 import Link from 'next/link'
@@ -199,9 +199,9 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
 
-        {/* <div className='pt-16'>
+        <div className='pt-16'>
           <Reviews />
-        </div> */}
+        </div>
       </section>
 
       <section>
